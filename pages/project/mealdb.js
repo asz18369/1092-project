d8282c81a8a9182ff31417468b9cad6a1fe2268b
aa870111 <asz18369@gmail.com>
2021-06-13 22:04:43 +0800

    Home page

--- pages/project/mealdb.js
+++ pages/project/mealdb.js
@@ -1,84 +1,67 @@
-import style from "../../styles/mealdb.module.css";
-import Head from "next/head";
-import mealData from "../../meal-data";
-import { useState } from "react";
+import styles from '../../styles/mealdb.module.css';
+import Head from 'next/head'
+//import mealData from './meal-data'
+import {useState} from 'react'
 
-export default function mealdb() {
-  const [input, setInput] = useState("");
-  const [mealData, setMealData] = useState([]);
-  const handleInput = (e) => {
-    setInput(e.target.value);
-  };
+export default function meals() {
+    const [input,setInput]=useState('')
+    const [mealData, setMealData] =useState([])
 
-  console.log(input);
+    const handleInput =(e) => {
+        setInput(e.target.value)
+    }
+    console.log(input)
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
-      .then((res) => res.json())
-      .catch((error) => console.error("Error", error))
-      .then((res) => {
-        console.log(res);
-        setMealData(res.meals);
-      });
-  };
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+        .then(res =>res.json())
+        .catch(error=>console.error('Error',error))
+        .then(res =>{
+            console.log(res)
+            setMealData(res.meals)
+        })
+    }
 
-  return (
+
+
+    return (
     <div>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css"
-        />
-        <title>MealDB</title>
-      </Head>
-      <div className={style.container}>
-        <h1 className={style.title__main}>Meal Finder</h1>
-        <div className={style["flex-box"]}>
-          <form id="submit" className={style["flex-box"]}>
-            <input
-              type="text"
-              value={input}
-              placeholder="Search for meals or keywords"
-              className={style.input}
-              onChange={handleInput}
-            />
-            <button
-              className={style.btn__search}
-              type="submit"
-              onClick={handleSubmit}
-            >
-              <i className="fas fa-search" />
+        <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+        <title>MealDb</title>
+        </Head>
+        <div className={styles.container}>
+        <h1 className={styles.title__main}>Meal Finder</h1>
+        <div className={styles["flex-box"]}>
+            <form id="submit" className={styles["flex-box"]}>
+                <input type="text" value={input} placeholder="Search for meals or keywords" className={styles.input} onChange={handleInput}/>
+                <button className={styles.btn__search} type="submit" onClick={handleSubmit}>
+                    <i className="fas fa-search"></i>
+                </button>
+                </form>
+                    <button className={styles.btn__random} id="random">
+                <i className="fas fa-random"></i>
             </button>
-          </form>
-          <button className={style.btn__random} id="random">
-            <i className="fas fa-random" />
-          </button>
         </div>
-        <div id="result-heading" className={style.title__search}>
-          <h1>Search Results for ' beef ' :</h1>
+        <div id="result-heading" className={styles.title__search}><h1>Search Results for ' {input} ' :</h1></div>
+        <div id="meals" className={styles.gallery}>
+            {mealData.map((meal) => (
+                <Meal key={meal.idMeal} name={meal.strMeal} image={meal.strMealThumb}/>
+            ))}
         </div>
-      </div>
-      <div id="meals" className={style.gallery}>
-        {mealData.map((meal) => (
-          <Meal
-            key={meal.idMeal}
-            name={meal.strMeal}
-            image={meal.strMealThumb}
-          ></Meal>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+        </div>
+    )
 }
 
-function Meal({ id, image, name }) {
-  return (
-    <div className={style["gallery-card"]}>
-      <img className={style["gallery-card__img"]} src={image} alt={name} />
-      <div className={style["gallery-card__details"]} data-mealid={id}>
-        <h2>{name}</h2>
-      </div>
-    </div>
-  );
+function Meal({id, image, name}) {
+    return(
+        <div className={styles["gallery-card"]}>
+            <img className={styles["gallery-card__img"]} src={image} alt={name} />
+                <div className={styles["gallery-card__details"]} data-mealid={id}> 
+                    <h2>{name}</h2>
+                    </div>
+            </div>
+    )
 }

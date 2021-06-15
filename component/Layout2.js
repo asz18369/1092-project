96406922a47d753a6f69dca5e6f2394531fd255c
aa870111 <asz18369@gmail.com>
2021-06-15 16:50:50 +0800

    word

--- component/Layout2.js
+++ component/Layout2.js
@@ -8,7 +8,7 @@ export default function Layout2() {
           <nav className={styles.nav}>
   <div className={styles.container}>
       <input className={styles.input} id="responsive-menu" type="checkbox" />
-      <label className={styles.label} for="responsive-menu">Menu <span className={styles["menu-icon"]}></span></label>
+      <label className={styles.label} for="responsive-menu">HOME PAGE <span className={styles["menu-icon"]}></span></label>
       <div className={styles.overlay}></div>
       <ul className={styles.ul}>
           <li className={styles.li}><a className={styles.a} href="http://art.yale.edu/">Yale University School of Art</a></li>
@@ -55,6 +55,14 @@ export default function Layout2() {
           </li>
       </ul>
   </div>
+
+  <main className={styles.main}>
+    <div className={styles.container}>
+        <h1 className={styles.h1}>Welcome to my project</h1>
+        <p className={styles.p}>Hi! I'm 陳至賢.And this is my final project,hope you like it and enjoy it.</p>
+    </div>
+</main>
+
   </nav>
   </div>
   )

96fd04409104801683a710344ba0b03f57763094
aa870111 <asz18369@gmail.com>
2021-06-15 17:02:07 +0800

    6/15

--- component/Layout2.js
+++ component/Layout2.js
@@ -11,16 +11,10 @@ export default function Layout2() {
       <label className={styles.label} for="responsive-menu">HOME PAGE <span className={styles["menu-icon"]}></span></label>
       <div className={styles.overlay}></div>
       <ul className={styles.ul}>
-          <li className={styles.li}><a className={styles.a} href="http://art.yale.edu/">Yale University School of Art</a></li>
-          <li className={styles.li}><a className={styles.a} href="http://www.suzannecollinsbooks.com/">Suzanne Collins</a></li>
-          <li className={styles.li}><a className={styles.a} href="http://www.arrestling.com/index.htm">Gulla’s Arrestling</a></li>
-          <li className={styles.li}><a className={styles.a} href="http://www.roverp6cars.com/">MGBD Parts &amp; Services</a></li>
-          <li className={styles.li}><a className={styles.a} href="http://www.irishwrecksonline.net/">Irish Shipwrecks Online</a></li>
-          <li className={styles.li}><a className={styles.a} href="http://www.arngren.net/">Arngren</a></li>
-          <li className={styles.li}><a className={styles.a} href="https://www.lingscars.com/">Ling's Cars</a></li>
-          <li className={styles.li}><a className={styles.a} href="http://www.007museum.com/">JAMES BOND 007 MUSEUM</a></li>
-          <li className={styles.li}><a className={styles.a} href="http://www.dokimos.org/ajff/">ACCEPT JESUS, FOREVER FORGIVEN!</a></li>
-          <li className={styles.li}><a className={styles.a} href="http://ronoslund.com/">Ron Oslund's Home Page</a></li>
+          <li className={styles.li}><a className={styles.a} >Meals</a></li>
+          <li className={styles.li}><a className={styles.a} >MealsDB</a></li>
+          <li className={styles.li}><a className={styles.a} >Straip</a></li>
+          <li className={styles.li}><a className={styles.a} >Project</a></li>
       </ul>
       <ul className={styles.ul} className={styles["social-media"]}>
           <li className={styles.li}>
